8294b69cd1f2db86f79900b17a21bd825ed94156
catherinegs <catherine.gourdinsg@gmail.com>
2022-03-28 20:20:47 +0200

    final project 2

--- analytics/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ analytics/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -2,32 +2,42 @@ package com.hemebiotech.analytics;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.TreeMap;
 
-import com.hemebiotech.analytics.ReadSymptomDataFromFile;
-
 public class AnalyticsCounter {
 	
-    public static void countFrequencies(ArrayList<String> result)
+    public static void countFrequencies(ArrayList<String> result) throws IOException
     {
-  
+        // Save original out stream.
+        PrintStream originalOut = System.out;
+
+        // Create a new file output stream.
+        PrintStream fileOut = new PrintStream("resultcorrect.out");
+        
+        // Redirect standard out to file.
+        System.setOut(fileOut);
+
         TreeMap<String, Integer> tmap = new TreeMap<String, Integer>();
+        
         for (String t : result) {
             Integer c = tmap.get(t);
             
             tmap.put(t, (c == null) ? 1 : c + 1);
         }
-  
-        for (Map.Entry m : tmap.entrySet())
-            System.out.println("Frequency of " + m.getKey() + " is " + m.getValue());
-               
-        
-    }
 
+        for (Map.Entry m : tmap.entrySet())
+            // If user input a valid email then write the email to ./out.txt and console.
+            System.out.println("Frequency of " + m.getKey() + " is " + m.getValue());
+        for (Map.Entry m : tmap.entrySet())
+        originalOut.println("Frequency of " + m.getKey() + " is " + m.getValue());
+
+        // Do not forget set original output and error stream back again.
+        System.setOut(originalOut);        
+    }  		
 	
 	public static void main(String args[]) throws Exception {
 		ArrayList<String> result = new ArrayList<String>();
@@ -46,9 +56,6 @@ public class AnalyticsCounter {
 		}
 		countFrequencies(result);
 
-
-		}
-
-	
+		}	
 	}
 
--- analytics/src/com/hemebiotech/analytics/SymptomLIst.java
+++ analytics/src/com/hemebiotech/analytics/SymptomLIst.java
@@ -1,9 +1,12 @@
 package com.hemebiotech.analytics;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -14,8 +17,13 @@ import java.util.TreeMap;
 
 public class SymptomLIst {
 	
-    public static void countFrequencies(ArrayList<String> result)
+    public static void countFrequencies(ArrayList<String> result) throws FileNotFoundException
     {
+        // Creating a File object that represents the disk file.
+        PrintStream o = new PrintStream(new File("A.txt"));
+  
+        // Store current System.out before assigning a new value
+        PrintStream console = System.out;
   
         TreeMap<String, Integer> tmap = new TreeMap<String, Integer>();
         for (String t : result) {
@@ -27,9 +35,19 @@ public class SymptomLIst {
             }
             
         }
+        // Assign o to output stream
+        System.setOut(o);
   
         for (Map.Entry m : tmap.entrySet())
             System.out.println("Frequency of " + m.getKey() + " is " + m.getValue());
+        
+        // Use stored value for output stream
+        System.setOut(console);
+        for (Map.Entry m : tmap.entrySet())
+
+        System.out.println("Frequency of " + m.getKey() + " is " + m.getValue());
+ 
+        
     }
 
 

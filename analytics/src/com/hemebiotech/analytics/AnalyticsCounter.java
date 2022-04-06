b3dbc2a3a9b7fb6a069ef7aea103a420dcc4d973
catherinegs <catherine.gourdinsg@gmail.com>
2022-04-06 21:46:43 +0200

    dernières finitions
    imports non utilisés supprimés

--- analytics/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ analytics/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,15 +1,8 @@
 package com.hemebiotech.analytics;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.PrintStream;
-import java.util.ArrayList;
+
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
-import java.util.TreeMap;
+
 
 public class AnalyticsCounter {
 	/**
@@ -26,11 +19,10 @@ public class AnalyticsCounter {
 		List<String> names = symptomFile.GetSymptoms();
 		
 		symptomFile.countSymptoms(names);
-		
-	    
-	        }	
+			    
+	 }	
 
-		}
+}
 
 
 
--- analytics/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
+++ analytics/src/com/hemebiotech/analytics/ReadSymptomDataFromFile.java
@@ -7,7 +7,6 @@ import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.TreeMap;
 
 /**
@@ -80,7 +79,7 @@ public class ReadSymptomDataFromFile implements ISymptomReader {
         System.setOut(originalOut);        
     }
 
-	}
+}
 
 
 

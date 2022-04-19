eaf9b4e09723b18ee566c5d7bb396b6fe120cea6
catherinegs <catherine.gourdinsg@gmail.com>
2022-04-19 13:09:21 +0200

    bis

--- analytics/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ analytics/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -9,63 +9,57 @@ import java.util.Map;
 import java.util.TreeMap;
 
 public class AnalyticsCounter implements IAnalyticsCounter {
-	
-	
+		
 	private String filePath;
-
 	public AnalyticsCounter(String filePath) {
 		
 		this.filePath = filePath;
 		// TODO Auto-generated constructor stub
 	}
-
 	/**
 	 * to count occurrences of elements of ArrayList 
 	 * @return 
-	 *
 	 */	
-	   @Override	
-	   public TreeMap<String, Integer>  countSymptoms() throws IOException
-	   {
-			ReadSymptomDataFromFile symptomFile = new ReadSymptomDataFromFile(filePath); 
+	
+	 @Override	
+	 public TreeMap<String, Integer>  countSymptoms() throws IOException {
+		 
+		ReadSymptomDataFromFile symptomFile = new ReadSymptomDataFromFile(filePath); 
 			
-			List<String> symptoms = symptomFile.getSymptoms();
-	        // TreeMap is created and elements of 
-	        // arraylist are inserted into it
-	        TreeMap<String, Integer> tmap = new TreeMap<String, Integer>();
+		List<String> symptoms = symptomFile.getSymptoms();
+	    // TreeMap is created and elements of 
+	    // arraylist are inserted into it
+	    TreeMap<String, Integer> tmap = new TreeMap<String, Integer>();
 	        
-	        for (String name : symptoms) {
+	       for (String name : symptoms) {
 	            Integer frequency = tmap.get(name);
 	            
 	            tmap.put(name, (frequency == null) ? 1 : frequency + 1);
 	        }
 	        return tmap;
-	    }
-	   
-		/**
-		 * to save and print result in txt file and the console 
-		 */		   	   
-	    public void saveFile(TreeMap<String, Integer> map) throws FileNotFoundException { 
+	 }	   
+	 /**
+      * to save and print result in txt file and the console 
+	  */		   	   
+	  public void saveFile(TreeMap<String, Integer> map) throws FileNotFoundException { 
 	        
-	        // Save original out stream.
-	        PrintStream originalOut = System.out;
+	     // Save original out stream.
+	     PrintStream originalOut = System.out;
 
-	        // Create a new file output stream.
-	        PrintStream fileOut = new PrintStream("result.out");
+	     // Create a new file output stream.
+	     PrintStream fileOut = new PrintStream("result.out");
 	        
-	        // Redirect standard out to file.
-	        System.setOut(fileOut);
+	     // Redirect standard out to file.
+	     System.setOut(fileOut);
 
-			for (Map.Entry m : map.entrySet())
+		    for (Map.Entry m : map.entrySet())
 	            // send frequency of symptoms to file result.out and console.
 	            System.out.println("Frequency of " + m.getKey() + " is " + m.getValue());
 	        for (Map.Entry m : map.entrySet())
 	        originalOut.println("Frequency of " + m.getKey() + " is " + m.getValue());
-
 	        // Do not forget set original output and error stream back again.
 	        System.setOut(originalOut);        
-
-        }
+      }
 }
 
 
